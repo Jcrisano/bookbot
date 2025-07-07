54c85d610fa89dea0655891d4419073665fb7311
Jcrisano <jcrisano90@gmail.com>
2025-07-07 16:48:13 -0400

    added new function to stats.py that counts each character using a dictionary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from stats import get_num_words, char_count
+
 
 def get_book_text(path):
     with open(path, 'r', encoding="utf-8") as f:
@@ -8,6 +9,10 @@ def get_book_text(path):
 def main():
     content = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(content)
+    character_count = char_count(content)
     print(f"{word_count} words found in the document")
+    print(character_count)
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,14 @@
 def get_num_words(book_content):
     word_list = book_content.split()
     return len(word_list)
+
+def char_count(book_content):
+    character_count_dict = {}
+    word_list = book_content.lower().split()
+    for word in word_list:
+        for letter in word:
+            if letter in character_count_dict:
+                character_count_dict[letter] += 1
+            else:
+                character_count_dict[letter] = 1
+    return character_count_dict
