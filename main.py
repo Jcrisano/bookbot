26868f5c6ce62ea80640d1074e0f52b3b8841074
Jcrisano <jcrisano90@gmail.com>
2025-07-08 15:39:29 -0400

    update sys.argv conditions and 2 books to the book folder

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from stats import get_num_words, char_count, sort_count, sort_on
-
+import sys
 
 def get_book_text(path):
     with open(path, 'r', encoding="utf-8") as f:
         book_contents = f.read()
     return book_contents
 
-book_path = "books/frankenstein.txt"
-
 def main():
+    # check to see if there are at least 2 arguments provided when program called
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        # if less than 2 arguments provided, program will terminate using sys.exit(1) indicating misc. error
+        sys.exit(1)
+        
+    book_path = sys.argv[1]
     content = get_book_text(book_path)
     word_count = get_num_words(content)
     character_count = char_count(content)
